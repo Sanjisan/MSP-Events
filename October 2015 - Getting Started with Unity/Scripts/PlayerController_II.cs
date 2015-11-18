958d4e1ed2e95d65628fdf52aaafa195106a93b3
sanjisan <mastahchef117@gmail.com>
2015-11-18 13:03:05 -0500

    PlayerController Update

--- October 2015 - Getting Started with Unity/Scripts/PlayerController_II.cs
+++ October 2015 - Getting Started with Unity/Scripts/PlayerController_II.cs
@@ -22,10 +22,13 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce (movement * speed);
     }
 
+    // Detects the collision. With our collider connector.
     void OnTriggerEnter(Collider other) 
     {
+        // Identifies the game object by comparing it to a string.
         if (other.gameObject.CompareTag ("Pick Up"))
         {
+            // Activates or deactivates the object. (Hides)
             other.gameObject.SetActive (false);
         }
     }
